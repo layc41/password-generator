86eb98ea4f58a00dff681bc72e69f29b5036ce3f
Claudia Lay <claudialay@claudias-mbp.home>
2020-06-14 22:42:44 -0500

    added description

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,94 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var randomLower = function() {
+  var lower = 'abcdefghijklmnopqrstuvwxyz';
+  return lower[Math.floor(Math.random() * lower.length)];
+}
+// console.log(randomLower());
+
+var randomUpper = function () {
+  var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return upper[Math.floor(Math.random() * upper.length)];
+}
+// console.log(randomUpper());
+
+var randomNumber = function () {
+  var num = '123456790';
+  return num[Math.floor(Math.random() * num.length)];
+}
+// console.log(randomNumber());
+
+var randomSpecial = function () {
+  var sym = '!@#$%^&*=-_';
+  return sym[Math.floor(Math.random() * sym.length)];
+}
+// console.log(randomSpecial());
+
+// Array of the functions that are true
+var randomFuncArr = []
+// console.log(randomFuncArr);
+
+// Call random function from random function array
+var randomFunc = function () {
+  for (var i = 0; i < randomFuncArr.length; i++) {
+   return randomFuncArr[Math.floor(Math.random() * randomFuncArr.length)]()
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Prompts for parameters related to password
+  var charCount = prompt("How many characters? (Between 8 - 128 characters)");
+    if (charCount < 8 | charCount > 128 | isNaN(charCount) == true) {
+      return alert("Invalid Input.");
+    }
+  var lowerCase = confirm("Do you want lowercase letters?");
+    if (lowerCase === true) {
+      randomFuncArr.push(randomLower);
+    }
+  var upperCase = confirm("Do you want uppercase letters?");
+    if (upperCase === true) {
+      randomFuncArr.push(randomUpper);
+    }
+  var num = confirm("Do you want numbers?");
+    if (num === true) {
+      randomFuncArr.push(randomNumber);
+    }
+  var specialChar = confirm("Do you want special characters?");
+    if (specialChar === true) {
+      randomFuncArr.push(randomSpecial);
+    }
+    else if (lowerCase === false && upperCase === false && num === false && specialChar == false) {
+      return alert("Please select valid characters.")
+    }
 
-  passwordText.value = password;
+  // Confirm what parameters are for password
+  // var passwordText = {
+  //   length: charCount,
+  //   lower: lowerCase,
+  //   upper: upperCase,
+  //   number: num,
+  //   special: specialChar
+  // }
+  // console.log(passwordText);
+
+  var password = ""
+
+  var generatePassword = function () {
+
+    for (var i = 0; i < charCount; i++) {
+      
+      password += randomFunc();
+      // console.log(randomFunc())
+    }
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  }
+
+  // generatePassword function
+  var password = generatePassword();
 
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Password Generator Starter Code
+# Password Generator
+
+
+# Horiseon-Project
+Refactored Horiseon website to improve accessibility. Horiseon is a social solution service organization. We optimize search engines to increase visibility and finding the right customers for your buiness, manage online reputation to control what potential customers search for you business, and help decide what social media platforms are best for producing leads for your business. Our processes and technology generate leads, increase brand awareness, and reduce advertising costs for your business to thrive.
+
+# Link to Deployed Application
+[Horiseon Website](https://layc41.github.io/Horiseon-Project/)
+
+# Screen Shots
+![Main](/Horiseon-Main.png)
+![Main](/Horiseon-screenshot2.png)
+![Main](/Horiseon-screenshot3.png)
